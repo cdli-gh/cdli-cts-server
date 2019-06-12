64d64e832429e8cb1d9f76d1e8bda7220db151d8
Ralph Giles <giles@thaumas.net>
2019-06-11 21:51:46 -0700

    Add Nemo corpus browser.
    Confirmed this shows the example corpora as expected.

--- app.py
+++ app.py
@@ -7,6 +7,7 @@ Following https://github.com/Capitains/tutorial-nemo
 import os
 
 from flask import Flask
+from flask_nemo import Nemo
 from capitains_nautilus.cts.resolver import NautilusCTSResolver
 
 flask_app = Flask("CTS webserver demo (nemo)")
@@ -15,6 +16,13 @@ corpora = [entry.path for entry in os.scandir('corpora') if entry.is_dir()]
 print('app: resolver given', corpora)
 resolver = NautilusCTSResolver(corpora)
 resolver.parse()
+
+nemo = Nemo(
+        name='Nemo',
+        app=flask_app,
+        resolver=resolver,
+        base_url=''
+)
 
 if __name__ == '__main__':
     flask_app.run(debug=True)
