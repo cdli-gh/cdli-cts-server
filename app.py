a0d860a00942846e9e7bea85c74cca292bb75384
Ralph Giles <giles@thaumas.net>
2019-06-11 21:28:39 -0700

    Add a NautilusCTSResolver.
    Still following the tutorial, add a Nautilus resolver.
    
    Point it at all the directories under corpora.
    
    Add the example lascivaroma corpora as git submodules. On a fresh
    checkout, run
    
        git submodule update --init
    
    to populate these directories.

--- app.py
+++ app.py
@@ -4,9 +4,17 @@ Serve the Canonical Text Services API for a collection of TEI xml files.
 Following https://github.com/Capitains/tutorial-nemo
 '''
 
+import os
+
 from flask import Flask
+from capitains_nautilus.cts.resolver import NautilusCTSResolver
 
 flask_app = Flask("CTS webserver demo (nemo)")
+
+corpora = [entry.path for entry in os.scandir('corpora') if entry.is_dir()]
+print('app: resolver given', corpora)
+resolver = NautilusCTSResolver(corpora)
+resolver.parse()
 
 if __name__ == '__main__':
     flask_app.run(debug=True)
