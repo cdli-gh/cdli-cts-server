a8b43ed04a4ef3f70aaf49329d027d4837c9460b
Ralph Giles <giles@thaumas.net>
2019-06-15 21:06:31 -0700

    Rename flask entrypoint to main.py.
    This matches the defaults in taingolo's docker service containers.

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ from flask_nemo import Nemo
 from capitains_nautilus.cts.resolver import NautilusCTSResolver
 from capitains_nautilus.flask_ext import FlaskNautilus
 
-flask_app = Flask("CTS webserver demo (nemo)")
+app = Flask("CTS webserver demo (nemo)")
 
 corpora = [entry.path for entry in os.scandir('corpora') if entry.is_dir()]
 print('app: resolver given', corpora)
@@ -20,16 +20,16 @@ resolver.parse()
 
 nautilus_api = FlaskNautilus(
         prefix='/api',
-        app=flask_app,
+        app=app,
         resolver=resolver
 )
 
 nemo = Nemo(
         name='Nemo',
-        app=flask_app,
+        app=app,
         resolver=resolver,
-        base_url=''
+        base_url='/nemo'
 )
 
 if __name__ == '__main__':
-    flask_app.run(debug=True)
+    app.run(debug=True)
