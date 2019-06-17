1834da56fed51e6c90031a99e181f2f920df75ef
Ralph Giles <giles@thaumas.net>
2019-06-17 11:27:40 -0700

    Add a basic landing page.
    In normal use this repo would probably be used to serve just
    the /api and /nemo routes, but for demonstration it's helpful
    to also provide a landing page for the site root so let people
    know those are available.

--- main.py
+++ main.py
@@ -31,5 +31,35 @@ nemo = Nemo(
         base_url='/nemo'
 )
 
+
+@app.route('/')
+def homepage():
+    '''Placeholder home page to help visitors.
+
+    We're mainly here to serve the nemo browser and api endoints,
+    but provide a simple landing page in case we're serving the
+    whole domain.'''
+    return '''<!doctype html>
+<html>
+  <head>
+    <meta charset=utf-8>
+    <title>CDLI CTS webservice</title>
+  </head>
+  <body>
+    <h1>CDLI Text Services</h1>
+    <p>This server provide texts from the
+       <a href=https://cdli.ucla.edu>Cuneiform Digital Library Initiative</a>.
+    </p>
+    <p>From here you can
+    <ul>
+      <li><a href=/nemo/>Browse the collection</a> with Nemo.</li>
+      <li><a href=/api/cts?request=GetCapabilities>Query the CTS api</a>
+          with Nautilus.</li>
+    </ul>
+    </p>
+  </body>
+</html>'''
+
+
 if __name__ == '__main__':
     app.run(debug=True)
