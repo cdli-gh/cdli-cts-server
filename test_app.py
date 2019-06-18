66f9ece83dff0e0c694e30ae4e2e360d16fc8ad7
Ralph Giles <giles@thaumas.net>
2019-06-18 08:40:57 -0700

    test: Add verification of the nemo service.
    Check that we can load the index page and that one of our test
    files shows in the listing. This will need to be updated if
    the collection details change.

--- test_app.py
+++ test_app.py
@@ -1,16 +1,30 @@
 import pytest
 
-from main import app
+import main
 
 
 @pytest.fixture
 def client():
-    app.config['TESTING'] = True
-    yield app.test_client()
+    main.app.config['TESTING'] = True
+    yield main.app.test_client()
 
 
 def test_index(client):
     '''Verify landing page.'''
     rv = client.get('/')
-    print(rv.data)
     assert 'CDLI Text Services' in rv.data.decode()
+
+
+def test_nemo(client):
+    '''Verify nemo pages.'''
+    nemo = main.nemo.prefix
+    print('Nemo prefix is', nemo)
+    rv = client.get(nemo, follow_redirects=True)
+    text = rv.data.decode()
+    assert 'Nemo' in text
+    assert 'Text Collections' in text
+
+    urn = 'urn:cts:cdli:test'
+    page = 'default-collection-atf2tei-test-examples'
+    text = client.get(f'{nemo}/collections/{urn}/{page}').data.decode()
+    assert 'belsunu' in text
